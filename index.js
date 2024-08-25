7e628e96425dda5c2d1b0fcfd2802fc4cd86e969
FrankOHara43 <111985913+FrankOHara43@users.noreply.github.com>
2024-08-25 15:54:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const url = require('url')
+
+// using callback 
+const server = http.createServer((req, res) => {
+   // console.log(req.url)
+   const pathName = req.url
+   if(pathName === '/' || pathName === '/overview') {
+    res.end("This is the overview!");
+   } else if(pathName === '/product') {
+    res.end("This is the product");
+   } else {
+    res.writeHead(404, {
+        'Content-type': 'text/html',
+        'my-own-header': 'hello-world'
+
+    })
+    res.end("<h1> page not found </h1>")
+   }
+
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000')
+})
+//localhost address = '127.0.0.1'
